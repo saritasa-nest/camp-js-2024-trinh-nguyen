7751e199203108263bd21c63e3e8d3ce399bac7a
trinhnguyen-saritasa <trinh.nguyen@saritasa.com>
2024-07-05 15:54:01 +0700

    undo some change JC-599

--- apps/react/src/features/genres/components/GenreCard/GenreCard.tsx
+++ apps/react/src/features/genres/components/GenreCard/GenreCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { memo, FC } from "react";
 import { Genre } from "@js-camp/core/models/genre";
 
 import styles from "./GenreCard.module.css";
@@ -9,11 +9,11 @@ interface Props {
 }
 
 /** Card with genre data. */
-const GenreCardComponent: React.FC<Props> = ({ genre }) => (
+const GenreCardComponent: FC<Props> = ({ genre }) => (
 	<div className={styles.card}>
 		<h2>{genre.name}</h2>
 		<span>Id - {genre.id}</span>
 	</div>
 );
 
-export const GenreCard = React.memo(GenreCardComponent);
+export const GenreCard = memo(GenreCardComponent);
