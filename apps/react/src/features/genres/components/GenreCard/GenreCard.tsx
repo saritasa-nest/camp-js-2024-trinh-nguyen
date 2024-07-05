0f23a2d7fdcb10d06f11ee0795dff3e98d8902ca
trinhnguyen-saritasa <trinh.nguyen@saritasa.com>
2024-07-05 15:43:36 +0700

    fix lint JC-599

--- apps/react/src/features/genres/components/GenreCard/GenreCard.tsx
+++ apps/react/src/features/genres/components/GenreCard/GenreCard.tsx
@@ -1,20 +1,19 @@
-import { memo, FC } from 'react';
-import { Genre } from '@js-camp/core/models/genre';
+import React from "react";
+import { Genre } from "@js-camp/core/models/genre";
 
-import styles from './GenreCard.module.css';
+import styles from "./GenreCard.module.css";
 
 interface Props {
-
 	/** Genre. */
 	readonly genre: Genre;
 }
 
 /** Card with genre data. */
-const GenreCardComponent: FC<Props> = ({ genre }) => (
+const GenreCardComponent: React.FC<Props> = ({ genre }) => (
 	<div className={styles.card}>
 		<h2>{genre.name}</h2>
 		<span>Id - {genre.id}</span>
 	</div>
 );
 
-export const GenreCard = memo(GenreCardComponent);
+export const GenreCard = React.memo(GenreCardComponent);
