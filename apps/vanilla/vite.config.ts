997e6b7d2c93b2c8f341dd1a7baa24d110f819bc
trinhnguyen-saritasa <trinh.nguyen@saritasa.com>
2024-07-16 13:31:07 +0700

    Add page in vite.config JC-625

--- apps/vanilla/vite.config.ts
+++ apps/vanilla/vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
       input: [
         resolve(root, 'index.html'),
 				resolve(root, 'book-catalog', 'index.html'),
+				resolve(root, 'black-jack', 'index.html'),
       ],
     },
   },
