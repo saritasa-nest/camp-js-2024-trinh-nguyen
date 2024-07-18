6e8dfff6101081402a15ca78312c9e0c667113be
trinhnguyen-saritasa <trinh.nguyen@saritasa.com>
2024-07-18 07:17:33 +0700

    Remove abstract JC-625

--- apps/vanilla/src/type-guards/is-button.ts
+++ apps/vanilla/src/type-guards/is-button.ts
@@ -2,6 +2,6 @@
  * Check an element is HTMLButton or not.
  * @param element The element need checking.
  */
-export function isHTMLButtonElement(element: HTMLElement | null): element is HTMLButtonElement {
+export function isHTMLButtonElement(element: unknown): element is HTMLButtonElement {
 	return element instanceof HTMLButtonElement;
 }
--- apps/vanilla/src/type-guards/is-element.ts
+++ apps/vanilla/src/type-guards/is-element.ts
@@ -3,6 +3,6 @@
  * Check an element is an HTMLElement or not.
  * @param element Element need checking.
  */
-export function isHTMLElement(element: HTMLElement | null | undefined): element is HTMLButtonElement {
+export function isHTMLElement(element: unknown): element is HTMLButtonElement {
 	return element instanceof HTMLElement;
 }
