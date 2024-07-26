0983bad077d9d5c9c5e53759c34cde0da60a2a3f
trinhnguyen-saritasa <trinh.nguyen@saritasa.com>
2024-07-26 09:51:59 +0700

    Fix deploy PR preview fail JC-640

--- apps/angular/src/environments/environment.prod.ts
+++ apps/angular/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 /** Production environment configuration. */
 export const environment = {
 	production: true,
+	api: 'https://api.camp-js.saritasa.rocks/api/v1/',
 };
--- apps/angular/src/environments/environment.ts
+++ apps/angular/src/environments/environment.ts
@@ -4,7 +4,6 @@
 /** Default environment configuration. */
 export const environment = {
 	production: false,
-	api: 'https://api.camp-js.saritasa.rocks/api/v1/',
 };
 
 /*
